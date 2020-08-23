d926e18d91b69149777b8896908fa41203fddef2
rajnish <rajnish.vit.0705@gmail.com>
2020-08-23 07:33:03 +0530

    my commit

--- src/main/java/com/hcl/App.java
+++ src/main/java/com/hcl/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Good Morning! Great!" );
+        System.out.println( "Hello World! Good Morning!" );
     }
 }
